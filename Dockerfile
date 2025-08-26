a36b597ce9361bd382d952571934579a353925bf
andretsilo <andrea.tsilogiannis@gmail.com>
2025-08-26 12:12:21 +0200

    Edit dockerfile for spark master and worker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM python:3.11
 
-WORKDIR /app
+WORKDIR /kafka/app
 
 COPY ./requirements.txt /code/requirements.txt
 
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
-COPY ./app /code/app
+COPY ./kafka/app /code/app
 
 CMD ["fastapi", "run", "/code/app/ingest/main.py", "--port", "80"]
---
+++ spark/spark_consumer.py
@@ -0,0 +1,23 @@
+import logging
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.cluster import Cluster
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+
+def create_spark_session():
+    spark = None
+
+    try:
+        spark = SparkSession.builder \
+            .appName('SensorFeedbackStream') \
+            .config("spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.13:3.5.1", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.0") \
+            .config("spark.cassandra.connection.host", "localhost") \
+            .getOrCreate()
+        
+        spark.sparkContext.set
+    except Exception as e:
+        logging.error("Could not create spark session: " + str(e))
+
+if __name__ == "__main__":
+    spark_session = create_spark_session()
+
